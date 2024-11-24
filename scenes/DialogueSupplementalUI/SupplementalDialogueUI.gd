81ae340682ab943134cd3c35ba547d04b14913d2
Manny-OnGit <manny.fluss@gmail.com>
2024-11-23 18:43:13 -0800

    example animations created

--- scenes/DialogueSupplementalUI/SupplementalDialogueUI.gd
+++ scenes/DialogueSupplementalUI/SupplementalDialogueUI.gd
@@ -2,8 +2,27 @@ extends CanvasLayer
 
 @onready var black_bar_animator: AnimationPlayer = %BlackBarAnimator
 
+@onready var christine_animation: AnimationPlayer = $ChristineAnimation
+@onready var nico_animation: AnimationPlayer = $NicoAnimation
+
+
+
+
+func _ready() -> void:
+	christine_animation.play("test_animation")
+	activate_black_bars()
+	nico_animation.play("test_animation")
+	await christine_animation.animation_finished
+	var tween : Tween = get_tree().create_tween()
+	tween.tween_property($NicoDupe,"position",Vector2(982,1536),5).set_ease(Tween.EASE_IN_OUT)
+	await tween.finished
+	christine_animation.play("do a backflip")
+	pass
+	
 #reciever for any commands
 func play_command(command : String)->void:
+	
+	
 	pass
 	
 	
